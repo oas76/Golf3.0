abdd19229a39cb5f840f52e0479ff5f8c48e04a7
oddaskaf <oddaskaf@cisco.com>
2019-03-04 11:04:19 +0100

    Moving prebuild to deploystep

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path')
+
+module.exports = {
+  entry: path.resolve(__dirname,'src/components/app.js'),
+  output: {
+    path: __dirname + '/src/server/static',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
