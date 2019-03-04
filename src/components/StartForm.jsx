d64fd868016440103f1eb48cba44d3dd9bed9343
oddaskaf <oddaskaf@cisco.com>
2019-03-04 09:05:07 +0100

    Changes

--- src/components/StartForm.jsx
+++ src/components/StartForm.jsx
@@ -19,7 +19,7 @@ class StartForm extends React.Component {
     handleInput = async (event) => {
         event.preventDefault();
         this.setState({pairingSize: event.target.value})
-        const resp = await axios.get(`http://${WEB_DOMAIN}/randomize?teamsize=${event.target.value}`);
+        const resp = await axios.get(`${WEB_DOMAIN}/randomize?teamsize=${event.target.value}`);
         this.props.onSubmit(resp.data)
     }
 
